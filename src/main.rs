616dfa9605135c7438ba1c8dd93a70cc1a4998a0
yosaka <fallingfluit@gmail.com>
2023-09-06 13:10:57 +0000

    post-01

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // 標準ライブラリにリンクしない
+#![no_main] // すべてのRustレベルのエントリポイントを無効にする
+
+use core::panic::PanicInfo;
+
+// no_mangle -> 名前修飾を無効に
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // リンカはデフォルトで_startという名前の関数を探すので
+    // この関数がエントリポイントとなる
+    loop {}
+}
+
+// panic時に呼ばれる関数
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
