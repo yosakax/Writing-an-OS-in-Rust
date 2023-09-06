0bd7ffaed870948ccea97bde3c59f3f36b2d3280
yosaka <fallingfluit@gmail.com>
2023-09-07 00:03:38 +0900

    post-02: run kernel with qemu

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,25 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"HEllo wOrld";
+
 // no_mangle -> 名前修飾を無効に
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // リンカはデフォルトで_startという名前の関数を探すので
     // この関数がエントリポイントとなる
+
+    // VGA bufferの位置の生ポインタ
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // シアン色
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
